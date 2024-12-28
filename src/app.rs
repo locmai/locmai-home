f8b8e61bf5ac4462aad3f43caaa963e36dfda9e0
locmai <locmai0201@gmail.com>
2024-12-29 01:58:20 +0700

    chore: init

---
+++ src/app.rs
@@ -0,0 +1,8 @@
+use leptos::prelude::*;
+
+#[component]
+pub fn App() -> impl IntoView {
+    view! {
+        <h1> Hello World! </h1>
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use app::*;
+use leptos::*;
+mod app;
+
+pub fn main() {
+    _ = console_log::init_with_level(log::Level::Debug);
+    console_error_panic_hook::set_once();
+
+    logging::log!("CSR mode - mounting to body");
+
+    mount::mount_to_body(|| {
+        view! { <App/> }
+    });
+}
